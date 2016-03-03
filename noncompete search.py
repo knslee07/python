147576f9b9b069fde221cfb5c45bcc3900c9aadb
knslee07 <knslee07@gmail.com>
2016-03-03 01:25:35 -0500

    Create noncompete search.py

---
+++ noncompete search.py
@@ -0,0 +1,6 @@
+# to do: log in to Mergent
+# 2. type noncompetes and divergent forms
+# 3. click and save all the links
+# 4. search the term and paragraph with the keyword.
+# 5. scrape the paragraph.
+# 6. save it to HDD.
