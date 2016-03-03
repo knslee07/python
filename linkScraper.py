ab102949c584261d192db6fd965c93da508026a0
knslee07 <knslee07@gmail.com>
2016-03-03 00:46:11 -0500

    Update linkScraper.py

--- linkScraper.py
+++ linkScraper.py
@@ -1,11 +1,15 @@
+import requests
+result=requests.get("http://oreilly.com/store/samplers.html")
+result.status_code
+c=result.content
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(c)
-samples = soup.find_all("a", "item-title")
+samples = soup.findAll("a", "item-title")
 samples[0]
 """<a class="item-title" href="http://cdn.oreilly.com/oreilly/booksamplers/9780596004927_sampler.pdf">
 Programming Perl </a>"""
 
 data = {}
 for a in samples:
-  title = a.string.strip()
-  data[title] = a.attrs['href']
+  title = a.string.strip()   # see how other parts are stripped from the html.
+  data[title] = a.attrs['href'] # only link addresses are extracted
